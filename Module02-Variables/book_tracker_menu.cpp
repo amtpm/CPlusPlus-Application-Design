1464dbe914b5191d045bf3425df67245e8be1a7d
Brian Molina <mail.brianmolina@gmail.com>
2026-09-08 22:30:57 -0700

    Add Module 2 menu

--- Module02-Variables/book_tracker_menu.cpp
+++ Module02-Variables/book_tracker_menu.cpp
@@ -2,41 +2,65 @@
 #include <string>
 using namespace std;
 
-void addRecord() {
-    cout << "Add Record selected." << endl;
+void addBook() {
+    cout << "Add Book selected." << endl;
 }
 
-void viewRecords() {
-    cout << "View Records selected." << endl;
+void removeBook() {
+    cout << "Remove Book selected." << endl;
 }
 
-void searchRecords() {
-    cout << "Search selected." << endl;
+void viewBooks() {
+    cout << "View Books selected." << endl;
+}
+
+void searchBooks() {
+    cout << "Search Books selected." << endl;
+}
+
+void markBookRead() {
+    cout << "Mark Book as Read selected." << endl;
+}
+
+void markBookUnread() {
+    cout << "Mark Book as Unread selected." << endl;
 }
 
 int main() {
     int choice = 0;
 
-    while (choice != 4) {
-        cout << "\n=== MY APPLICATION ===" << endl;
-        cout << "1. Add Record" << endl;
-        cout << "2. View Records" << endl;
-        cout << "3. Search" << endl;
-        cout << "4. Exit" << endl;
+    while (choice != 7) {
+        cout << "\n=== Book Tracker ===" << endl;
+        cout << "1. Add Book" << endl;
+        cout << "2. View Books" << endl;
+        cout << "3. Search Books" << endl;
+        cout << "4. Remove Book" << endl;
+        cout << "5. Mark Book as Read" << endl;
+        cout << "6. Mark Book as Unread" << endl;
+        cout << "7. Exit" << endl;
         cout << "Choose an option: ";
         cin >> choice;
 
         switch (choice) {
             case 1:
-                addRecord();
+                addBook();
                 break;
             case 2:
-                viewRecords();
+                viewBooks();
                 break;
             case 3:
-                searchRecords();
+                searchBooks();
                 break;
             case 4:
+                removeBook();
+                break;
+            case 5:
+                markBookRead();
+                break;
+            case 6:
+                markBookUnread();
+                break;
+            case 7:
                 cout << "Goodbye!" << endl;
                 break;
             default:
